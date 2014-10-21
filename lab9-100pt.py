1ffc9f625d420e87fc717b9fad04d3ffb07cc345
hunterxpatterson <hunterxpatterson@gmail.com>
2014-10-21 13:32:04 -0500

    Finished 100pt

--- lab9-100pt.py
+++ lab9-100pt.py
@@ -13,3 +13,25 @@
 # - A temperature of over 105F
 # - A temperature of over 102F and they have been sick in the last 24 hours
 # - A temperature over 100, OR they've been sick in the last 24 hours, AND they've recently travelled to West Africa.
+
+
+ongoing = True
+while (ongoing == True):
+    print 'Type your temperature'
+    userinput = int(raw_input())
+    print 'Have you been sick in the last 24 hours?'
+    userinput2 = raw_input()
+    print 'Have you been to West Africa recently?'
+    userinput3 = raw_input()
+    if userinput > 105:
+        print 'You need to go to the hospital!'
+    if userinput > 102 and userinput2 == 'yes':
+        print 'You need to go to the hospital!'
+    if userinput > 100 or userinput2 == 'yes' and userinput3 == 'yes':
+        print 'You need to go to the hospital!'
+        print 'Are there any more patients?'
+        userinput4 = raw_input()
+    print '--------------------------------------------'
+    if userinput4 == 'no':
+        ongoing = False
+
